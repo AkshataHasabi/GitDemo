ca0615787e06fe156eaedd6ed3e5ae3f6434c982
AkshataHasabi <akshuh818@gmail.com>
2021-12-08 03:39:33 -0800

    firs1 commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("HelloJava");
 		System.out.println("HelloJava1");
+		System.out.println("HelloJava2");
+		System.out.println("HelloJava3");
 		
 	}
 }
